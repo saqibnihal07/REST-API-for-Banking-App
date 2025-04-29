ed01bcdec7fdc89b300fd39a1b081ac12981bd27
saqibnihal07 <saqibnihal378@gmail.com>
2025-04-29 06:35:28 +0530

    Delete banking-app directory

--- banking-app/banking-app/src/main/java/net/java/banking/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package net.java.banking.repository;
-
-import net.java.banking.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-
-}
